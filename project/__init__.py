a48a3d0c7c97c05a5ada1bffb1f1b8da561fce1f
Leopoldo L <leopoldo.lara@laso-consulting.com>
2017-12-19 14:51:49 -0600

    Initial commit

---
+++ project/__init__.py
@@ -0,0 +1,13 @@
+# project/__init__.py
+from flask import Flask,jsonify
+
+# instantiate the app
+app = Flask(__name__)
+
+# set config
+app.config.from_object('project.config.DevelopmentConfig')
+
+@app.route('/ping', methods=['GET'])
+def ping_ping():
+	return jsonify({'status':'success','message':'pong!'})
+	
